eccad2778cc3a34aaf1af403eaaa2210be3be434
Rajnish Deshmukh <rajnishdeshmukh05@gmail.com>
2023-03-14 17:47:10 +0530

    navigation sidebar hide show functionality added using redux and some styling

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,7 @@ import Sidebar from './Sidebar';
 
 const Body = () => {
   return (
-    <div className="grid grid-flex-col">
+    <div className="flex">
       <Sidebar />
       <MainContainer />
     </div>
--- src/components/Head.js
+++ src/components/Head.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleMenu } from '../utils/appSlice';
 
 const Head = () => {
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
       <div className="flex col-span-1">
         <img
-          className="h-8"
+          className="h-8 cursor-pointer"
           src="https://cdn-icons-png.flaticon.com/512/3917/3917215.png"
           alt="Hamberger Menu icon"
+          onClick={() => toggleMenuHandler()}
         />
-        <img
-          className="h-8 mx-2"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
-          alt="youtube logo"
-        />
+        <a href="/">
+          <img
+            className="h-8 mx-2"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+            alt="youtube logo"
+          />
+        </a>
       </div>
       <div className="col-span-10 px-10">
         <input

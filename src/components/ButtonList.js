dfccd50772b4de13021a675d715f035a19f9a2c6
Rajnish Deshmukh <rajnishdeshmukh05@gmail.com>
2023-03-15 12:10:57 +0530

    fetch videos from youtube api and styled video container component.

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Button from './Button';
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  return (
+    <div className="flex">
+      <Button name="All" />
+      <Button name="Gaming" />
+      <Button name="Songs" />
+      <Button name="Live" />
+      <Button name="Cricket" />
+      <Button name="Movies" />
+      <Button name="React" />
+      <Button name="News" />
+      <Button name="Live" />
+      <Button name="Debates" />
+      <Button name="Awards" />
+      <Button name="Watched" />
+    </div>
+  );
 };
 
 export default ButtonList;
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { API_YOUTUBE_VIDEO } from '../utils/constants';
+import VideoCard from './VideoCard';
 
 const VideoContainer = () => {
-  return (
-    <div>VideoContainer</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
 
-export default VideoContainer
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(API_YOUTUBE_VIDEO);
+    const json = await data.json();
+    setVideos(json.items);
+  };
+
+  return (
+    <div className="flex flex-wrap">
+      {videos.map((video) => (
+        <VideoCard key={video.id} info={video} />
+      ))}
+    </div>
+  );
+};
+
+export default VideoContainer;
